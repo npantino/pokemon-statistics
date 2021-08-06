534bc9a8610e1727d6353d61ac4d5ec7a7a08dbc
Sean Rester <syntastical@users.noreply.github.com>
2021-08-06 18:21:58 -0400

    Update script.js

--- Pokemon front-end/script.js
+++ Pokemon front-end/script.js
@@ -2,9 +2,9 @@ function getStuff() {
     let pokemonName = document.getElementById("searchBox").value;
     let id = ""
     let location_details = ""
-    document.getElementById("gameInfo").style.display = "block";
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`, {
+    // Collect promises so we can wait to show the info box until all promises have resolved
+    const pokemonPromise = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`, {
         headers: {
             'Content-Type': 'application/json',
             credentials: 'include',
@@ -13,21 +13,21 @@ function getStuff() {
         .then(response => {
             return response.json()
                 .then(json => {
-                    id = JSON.stringify(json.id.toString()).replace(/['"]+/g, '');
-                    document.getElementById("id").innerHTML = "id: " + id;
+                    id = json.id;
+                    document.getElementById("id").innerHTML = `id: ${id}`;
 
                     let total = 0;
                     for (let i = 0; i < json.stats.length; i++) {
                         console.log(json.stats[i].base_stat);
                         total += json.stats[i].base_stat;
-                        document.getElementById("stat_" + (i+1).toString()).innerHTML = json.stats[i].base_stat;
+                        document.getElementById("stat_" + (i+1)).innerHTML = json.stats[i].base_stat;
                     }
                     document.getElementById("stat_total").innerHTML = total;
                 });
         });
 
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/encounters`, {
+    const encounterPromise = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/encounters`, {
         headers: {
             'Content-Type': 'application/json',
             credentials: 'include',
@@ -39,8 +39,7 @@ function getStuff() {
                     for (let i = 0; i < json.length; i++) {
                         let area = json[i].location_area.name;
                         for (let j = 0; j < json[i].version_details.length; j++) {
-                            let gameName = "";
-                            gameName = json[i].version_details[j].version.name;
+                            let gameName = json[i].version_details[j].version.name;
                             location_details += gameName
                             if (j === json[i].version_details.length - 1) {
                                 location_details += ": "
@@ -49,12 +48,16 @@ function getStuff() {
                                 location_details += ", "
                             }
                         }
-                        location_details += area + "<br>";
+                        location_details += `${area} <br>`;
                     }
                     console.log(location_details);
-                    location_details = JSON.stringify(location_details).replace(/['"]+/g, '');
                     document.getElementById('locations').innerHTML = location_details;
 
                 });
         });
+
+    // Once all promises have resolved, show the info box.
+    Promise.all([pokemonPromise, encounterPromise]).then(() => {
+        document.getElementById("gameInfo").style.display = "block";
+    })
 }
